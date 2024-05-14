5de78f46b4b914947067defcd3d04720c927474c
Wattersto08 <tom.j.watters@gmail.com>
2024-05-14 23:25:20 +0100

    kml parser, onject for kml features

---
+++ .ipynb_checkpoints/kml_object-checkpoint.py
@@ -0,0 +1,13 @@
+class Object:
+    def __init__(self, name, coords, Type):
+        self.name = name
+        self.coords = coords
+        self.Type = Type
+        
+    def __str__(self):
+        return f"{self.name} ({self.Type})"
+    
+    def dumpdata(self):
+        print(self.name)
+        print(self.coords)
+        print(self.Type)
---
+++ kml_parser_functs.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import random as rd
+import bs4 as bs
+from bs4 import BeautifulSoup
+import kml_object 
+
+def parse_coord_string(coord_string):
+    longitudes = []
+    latitudes = []
+
+    coord_string = coord_string.replace('\n','')
+    coord_string = coord_string.replace('\t','')
+    coord_string = coord_string.replace(' ',',')
+    coord_string = coord_string[:-1]
+    coords = coord_string.split(',')
+
+
+    for i in range(0,len(coords),3):
+        longitudes.append(coords[i])
+    for i in range(1,len(coords),3):
+        latitudes.append(coords[i])
+        
+    op = []
+    for i in range(len(longitudes)):
+        op.append([longitudes[i],latitudes[i]])
+    
+    return op
+
+
+def parse_KML(filename):
+    file = open(filename, "r")
+    contents = file.read()
+    soup = BeautifulSoup(contents, 'xml')
+    names = soup.find_all('name')
+
+    features = soup.find_all('Placemark')
+
+    #process names
+    project_name = str(names[0])[10:-11]
+
+    obj_list = []
+
+    for feature in features:
+        
+        featurename = str(feature.find_all('name'))[11:-12]
+        coords = parse_coord_string(feature.coordinates.text)
+
+        if feature.find('Polygon'):
+            featuretype = 'polygon'
+
+        if feature.find('Point'):
+            featuretype = 'Point'
+
+        if feature.find('LineString'):
+            featuretype = 'LineString'
+        
+        obj_list.append(kml_object.Object(featurename,coords,featuretype))
+        
+    return project_name, obj_list
+
